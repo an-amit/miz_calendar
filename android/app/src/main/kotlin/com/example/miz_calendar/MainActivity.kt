2c64adf7e660565eb289b0bcb5d094cea9ed4e23
an-amit <ambedkaramit176@gmail.com>
2026-09-14 17:39:33 +0530

    fixed the app package mismatch and build installing issue

--- android/app/src/main/kotlin/com/example/miz_calendar/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.miz_calendar
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/mizancalendar/miz_calendar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mizancalendar.miz_calendar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
